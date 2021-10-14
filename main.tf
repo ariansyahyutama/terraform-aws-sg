02ac6102f16f3a406e42fd32c32e7cb2354c0aa1
ariansyahyutama <67248935+ariansyahyutama@users.noreply.github.com>
2021-10-14 07:37:51 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,85 @@
+# Get ID of created Security Group
+locals {
+  this_sg_id = var.create_sg ? concat(aws_security_group.this.*.id, [""])[0] : var.security_group_id
+  #this_sg_id = var.create_sg ? concat(aws_security_group.this.*.id, aws_security_group.this_name_prefix.*.id, [""])[0] : var.security_group_id
+}
+
+# Security group with name
+resource "aws_security_group" "this" {
+  #count = var.create && var.create_sg && !var.use_name_prefix ? 1 : 0
+  count = var.create && var.create_sg ? 1 : 0
+
+  name                   = var.name
+  description            = var.description
+  vpc_id                 = var.vpc_id
+  revoke_rules_on_delete = var.revoke_rules_on_delete
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+  )
+}
+
+# Ingress - List of rules (simple)
+# Security group rules with "cidr_blocks" and it uses list of rules names
+resource "aws_security_group_rule" "ingress_rules" {
+  count = var.create ? length(var.ingress_rules) : 0
+
+  security_group_id = local.this_sg_id
+  type              = "ingress"
+
+  cidr_blocks      = var.ingress_cidr_blocks
+  ipv6_cidr_blocks = var.ingress_ipv6_cidr_blocks
+  prefix_list_ids  = var.ingress_prefix_list_ids
+  description      = var.rules[var.ingress_rules[count.index]][3]
+
+  from_port = var.rules[var.ingress_rules[count.index]][0]
+  to_port   = var.rules[var.ingress_rules[count.index]][1]
+  protocol  = var.rules[var.ingress_rules[count.index]][2]
+}
+
+
+# Egress - List of rules (simple)
+# Security group rules with "cidr_blocks" and it uses list of rules names
+resource "aws_security_group_rule" "egress_rules" {
+  count = var.create ? length(var.egress_rules) : 0
+
+  security_group_id = local.this_sg_id
+  type              = "egress"
+
+  cidr_blocks      = var.egress_cidr_blocks
+  ipv6_cidr_blocks = var.egress_ipv6_cidr_blocks
+  prefix_list_ids  = var.egress_prefix_list_ids
+  description      = var.rules[var.egress_rules[count.index]][3]
+
+  from_port = var.rules[var.egress_rules[count.index]][0]
+  to_port   = var.rules[var.egress_rules[count.index]][1]
+  protocol  = var.rules[var.egress_rules[count.index]][2]
+}
+
+/*
+#################################
+# Security group with name_prefix
+#################################
+resource "aws_security_group" "this_name_prefix" {
+  count = var.create && var.create_sg && var.use_name_prefix ? 1 : 0
+
+  name_prefix            = "${var.name}-"
+  description            = var.description
+  vpc_id                 = var.vpc_id
+  revoke_rules_on_delete = var.revoke_rules_on_delete
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags,
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+*/
---
+++ variables.tf
@@ -0,0 +1,174 @@
+#################
+# Security group
+#################
+variable "create" {
+  description = "Whether to create security group and all rules"
+  type        = bool
+  default     = true
+}
+
+variable "create_sg" {
+  description = "Whether to create security group"
+  type        = bool
+  default     = true
+}
+
+variable "security_group_id" {
+  description = "ID of existing security group whose rules we will manage"
+  type        = string
+  default     = null
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where to create security group"
+  type        = string
+  default     = null
+}
+
+variable "name" {
+  description = "Name of security group - not required if create_group is false"
+  type        = string
+  default     = null
+}
+
+variable "use_name_prefix" {
+  description = "Whether to use name_prefix or fixed name. Should be true to able to update security group name after initial creation"
+  type        = bool
+  default     = true
+}
+
+variable "description" {
+  description = "Description of security group"
+  type        = string
+  default     = "Security Group managed by Terraform"
+}
+
+variable "revoke_rules_on_delete" {
+  description = "Instruct Terraform to revoke all of the Security Groups attached ingress and egress rules before deleting the rule itself. Enable for EMR."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to security group"
+  type        = map(string)
+  default     = {}
+}
+
+##########
+# Ingress
+##########
+variable "ingress_rules" {
+  description = "List of ingress rules to create by name"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_with_self" {
+  description = "List of ingress rules to create where 'self' is defined"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_with_cidr_blocks" {
+  description = "List of ingress rules to create where 'cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_with_ipv6_cidr_blocks" {
+  description = "List of ingress rules to create where 'ipv6_cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_with_source_security_group_id" {
+  description = "List of ingress rules to create where 'source_security_group_id' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "ingress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all ingress rules"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_ipv6_cidr_blocks" {
+  description = "List of IPv6 CIDR ranges to use on all ingress rules"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_prefix_list_ids" {
+  description = "List of prefix list IDs (for allowing access to VPC endpoints) to use on all ingress rules"
+  type        = list(string)
+  default     = []
+}
+
+########
+# Egress
+#########
+variable "egress_rules" {
+  description = "List of egress rules to create by name"
+  type        = list(string)
+  default     = []
+}
+
+variable "egress_with_self" {
+  description = "List of egress rules to create where 'self' is defined"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_with_cidr_blocks" {
+  description = "List of egress rules to create where 'cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_with_ipv6_cidr_blocks" {
+  description = "List of egress rules to create where 'ipv6_cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_with_source_security_group_id" {
+  description = "List of egress rules to create where 'source_security_group_id' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all egress rules"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "egress_ipv6_cidr_blocks" {
+  description = "List of IPv6 CIDR ranges to use on all egress rules"
+  type        = list(string)
+  default     = ["::/0"]
+}
+
+variable "egress_prefix_list_ids" {
+  description = "List of prefix list IDs (for allowing access to VPC endpoints) to use on all egress rules"
+  type        = list(string)
+  default     = []
+}
+
+
+#rules
+variable "rules" {
+  description = "Map of known security group rules (define as 'name' = ['from port', 'to port', 'protocol', 'description'])"
+  type        = map(list(any))
+
+  # Protocols (tcp, udp, icmp, all - are allowed keywords) or numbers (from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml):
+  # All = -1, IPV4-ICMP = 1, TCP = 6, UDP = 17, IPV6-ICMP = 58
+  default = {
+# RDP
+    rdp-tcp = [3389, 3389, "tcp", "Remote Desktop"]
+    rdp-udp = [3389, 3389, "udp", "Remote Desktop"]
+
+  }
+}
